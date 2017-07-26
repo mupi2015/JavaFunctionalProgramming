0e0a284b55d22af4243f4829d0ece5a39d4704ac
arkaprovob <arkaprovo.bhattacharjee@sath.com>
2017-07-26 08:59:46 +0530

    added .gitignore and minor alterations

--- src/test/java/ExtendedBiFunction.java
+++ src/test/java/ExtendedBiFunction.java
@@ -5,14 +5,14 @@ public interface ExtendedBiFunction<T, U, R>extends BiFunction<T, U, R> {
 
     R apply(T t, U u);
 
+    /*Its line return new Function(){
+         @Override
+         R apply(T t){
+             return t;
+        }
+     }*/
     default Function<U, R> curry1(T t) {
         System.out.println("Parameter passed to the main function is "+t);
-        //Its line return new Function(){
-        //      @Override
-        //      R apply(T t){
-        //         return t;
-        //    }
-        // }
         return (U u) -> {
             System.out.println("Passing t: "+t+" and u :"+u+" to BiFunction's R apply(T t, U u)");
             return apply(t, u);
